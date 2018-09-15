595b3cec1bb377406ade6322e2df8a90ba10af53
Ambrogio Piredda <ambrogiopiredda@gmail.com>
2018-09-15 22:54:20 +0200

    modificado:    events-as-posts.php

--- events-as-posts.php
+++ events-as-posts.php
@@ -11,6 +11,7 @@ Domain Path:  /languages
 License:      GPLv2 or later
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 */
+
 /*
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
